96422137ea9443f4abf87c65dcec5904b76dba06
Yamamoto30 <miolacrosse3@gmail.com>
2025-04-16 19:30:22 +0900

    Update WaterIntakeService.java

--- src/main/java/com/example/demo/service/WaterIntakeService.java
+++ src/main/java/com/example/demo/service/WaterIntakeService.java
@@ -1,10 +1,24 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.entity.WaterIntake;
 
 public interface WaterIntakeService {
 
 	void regist(WaterIntake review);
 	
+	//全ての水分摂取データを取得
+    List<WaterIntake> getAllWaterIntakes();
+    
+    //指定されたIDの水分摂取データを削除
+    void delete(Long id);
+    
+ // IDを指定して1件のデータを取得
+    WaterIntake findById(Long id);
+
+    // データの更新
+    void update(WaterIntake intake);
+
 }
 
