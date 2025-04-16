97edbbcf28b513151314b816faff1ffe49cdf55f
Yamamoto30 <miolacrosse3@gmail.com>
2025-04-16 19:29:05 +0900

    Update WaterIntakeRepository.java

--- src/main/java/com/example/demo/repository/WaterIntakeRepository.java
+++ src/main/java/com/example/demo/repository/WaterIntakeRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.example.demo.entity.WaterIntake;
 
 public interface WaterIntakeRepository {
-
-	void add(WaterIntake review);
-	
+    void save(WaterIntake waterIntake);
+    List<WaterIntake> findAll();
+    void deleteById(Long id);
+    Optional<WaterIntake> findById(Long id);  // 追加
 }
+
