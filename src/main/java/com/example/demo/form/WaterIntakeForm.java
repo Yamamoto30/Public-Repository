165bd7a02a4201ac7f04d9bfb0e822e336dceed2
Yamamoto30 <miolacrosse3@gmail.com>
2025-04-18 23:44:39 +0900

    Update WaterIntakeForm.java

--- src/main/java/com/example/demo/form/WaterIntakeForm.java
+++ src/main/java/com/example/demo/form/WaterIntakeForm.java
@@ -6,26 +6,23 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class WaterIntakeForm {
-	
-	// ←★ 追加！編集時に必要
+    
+    // 編集時に必要なID
     private Long id;
-
-	@NotNull(message="ユーザーIDを入力してください。")
-	@Size(min=4, max=16, message="4文字から16文字で指定してください。")
-	private String userId;
-
-	@NotNull(message = "日付を入力してください。")
-	@Past(message="今日以前の日付を入力してください。")
-	private Date intakeDate;
-
-	@NotNull(message="摂取量を入力してください。")
-	@Min(value=10, message="10ml以上を入力してください。")
-	@Max(value=5000, message="5000ml以下を入力してください。")
-	private Integer amount;
-
+    
+    // ユーザーID（画面からは非表示）
+    private String userId;
+    
+    @NotNull(message = "日付を入力してください。")
+    @Past(message="今日以前の日付を入力してください。")
+    private Date intakeDate;
+    
+    @NotNull(message="摂取量を入力してください。")
+    @Min(value=10, message="10ml以上を入力してください。")
+    @Max(value=5000, message="5000ml以下を入力してください。")
+    private Integer amount;
 }
